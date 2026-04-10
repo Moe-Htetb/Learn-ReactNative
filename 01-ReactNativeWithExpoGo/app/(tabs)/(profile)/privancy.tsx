7937770aae2eda7fd637a1bc7407866e30afae50
Moe-Htetb <moehtetoo2005@gmail.com>
2026-04-10 15:52:20 +0630

    feat: configure header

--- 01-ReactNativeWithExpoGo/app/(tabs)/(profile)/privancy.tsx
+++ 01-ReactNativeWithExpoGo/app/(tabs)/(profile)/privancy.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 export default function Privancy() {
   return (
     <View>
+      <Stack.Screen
+        options={{
+          title: "Privancy",
+          headerRight: () => <Button title="login"></Button>,
+        }}
+      />
       <Text>privancy</Text>
     </View>
   );

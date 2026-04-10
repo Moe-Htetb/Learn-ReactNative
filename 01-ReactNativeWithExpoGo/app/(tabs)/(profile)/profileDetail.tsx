8281f2ae14f165ad171ca329ffa251884ce3b47c
Moe-Htetb <moehtetoo2005@gmail.com>
2026-04-10 11:30:45 +0630

    feat: router push and replace and navigate

--- 01-ReactNativeWithExpoGo/app/(tabs)/(profile)/profileDetail.tsx
+++ 01-ReactNativeWithExpoGo/app/(tabs)/(profile)/profileDetail.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { Link, useLocalSearchParams, useRouter } from "expo-router";
 
 export default function ProfileDetail() {
+  const router = useRouter();
+  const { username } = useLocalSearchParams();
   return (
     <View>
-      <Text>ProfileDetail</Text>
+      <Text>ProfileDetail - {username}</Text>
+      <Link href="/privancy">go to privancy navigate</Link>
+      <Link href="/profileDetail" push>
+        go to privancy navigate push
+      </Link>
+      <Link href="/privancy" replace>
+        go to privancy navigate replace
+      </Link>
+
+      <Button
+        title="go to privancy navigate"
+        onPress={() => router.navigate("/privancy")}
+      />
+      <Button
+        title="go to privancy push"
+        onPress={() =>
+          router.push({
+            pathname: "/profileDetail",
+            params: { username: "moehtet" },
+          })
+        }
+      />
+      <Button
+        title="go to privancy replace"
+        onPress={() => router.replace("/privancy")}
+      />
     </View>
   );
 }

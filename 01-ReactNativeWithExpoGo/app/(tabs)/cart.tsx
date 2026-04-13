e595a29a9c031bba982545f091b4f5e34330b864
Moe-Htetb <moehtetoo2005@gmail.com>
2026-04-13 22:00:10 +0630

    feat: expo image with dev build

--- 01-ReactNativeWithExpoGo/app/(tabs)/cart.tsx
+++ 01-ReactNativeWithExpoGo/app/(tabs)/cart.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import {
   //   SafeAreaView,
@@ -10,15 +10,34 @@ export default function CartScreen() {
   const insets = useSafeAreaInsets();
   return (
     //<SafeAreaView></SafeAreaView> can use this instead of View
-    <View style={{ flex: 1, paddingTop: insets.top }}>
-      <Text>CartScreen</Text>
-      <Image
-        source={
-          // uri: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-          require("../../assets/images/react-logo.png")
-        }
-        style={{ width: 200, height: 200 }}
-      />
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, paddingTop: insets.top }}>
+        <Text>CartScreen</Text>
+        <Image
+          source={
+            // uri: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            require("../../assets/images/react-logo.png")
+          }
+          style={{ width: 200, height: 200 }}
+        />
+        <Image
+          // source={require("../../assets/images/light.png")}
+          source={{ uri: "light " }}
+          style={{ width: 200, height: 200 }}
+        />
+        {/* <Image
+          source={require("../../assets/images/light.png")}
+          style={{ width: 200, height: 200 }}
+        />
+        <Image
+          source={require("../../assets/images/light.png")}
+          style={{ width: 200, height: 200 }}
+        />
+        <Image
+          source={require("../../assets/images/light.png")}
+          style={{ width: 200, height: 200 }}
+        /> */}
+      </View>
+    </ScrollView>
   );
 }

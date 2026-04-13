9b7d122e903b93a568f13730407f456a6cf87abd
Moe-Htetb <moehtetoo2005@gmail.com>
2026-04-13 19:38:46 +0630

    feat: font and image

--- 01-ReactNativeWithExpoGo/app/(tabs)/cart.tsx
+++ 01-ReactNativeWithExpoGo/app/(tabs)/cart.tsx
@@ -4,6 +4,7 @@ import {
   //   SafeAreaView,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
+import { Image } from "expo-image";
 
 export default function CartScreen() {
   const insets = useSafeAreaInsets();
@@ -11,6 +12,13 @@ export default function CartScreen() {
     //<SafeAreaView></SafeAreaView> can use this instead of View
     <View style={{ flex: 1, paddingTop: insets.top }}>
       <Text>CartScreen</Text>
+      <Image
+        source={
+          // uri: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          require("../../assets/images/react-logo.png")
+        }
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }
